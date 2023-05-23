9115d8db8436b30308781fe73980b909830f7c51
Felix <81219766+idkfelix@users.noreply.github.com>
2023-05-23 13:05:44 +1000

    init

---
+++ EdroloAutoAnwserV1.user.js
@@ -0,0 +1,47 @@
+// ==UserScript==
+// @name         Edrolo Auto Anwser
+// @author       idkfelix
+// @namespace    http://tampermonkey.net/
+// @version      1.0.0
+// @description  Press f2 to automatically select the correct option in a multiple-choice question and click the Save & continue button
+// @match        *://edrolo.com.au/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const waitForElement = (selector, callback) => {
+        const observer = new MutationObserver((mutationsList, observer) => {
+            const element = document.querySelector(selector);
+            if (element) {
+                callback(element);
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    };
+
+    const runScript = () => {
+        waitForElement('.input-wrapper.is-correct input[type="radio"]', (correctOption) => {
+            if (correctOption) {
+                correctOption.click();
+                waitForElement('button.btn.btn-default', (saveButton) => {
+                    if (saveButton) {
+                        saveButton.click();
+                    }
+                });
+            }
+        });
+    };
+
+    document.addEventListener('keydown', (event) => {
+        if (event.keyCode === 113) { // F2 key
+            runScript();
+        }
+    });
+})();
